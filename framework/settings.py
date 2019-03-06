d4bd6a45b60403bf9616b5eef155cf177a3de9ce
aswinshenoy <aswinshenoy65@gmail.com>
2019-03-06 23:31:52 +0530

    Fix Heroku

--- framework/settings.py
+++ framework/settings.py
@@ -177,7 +177,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, 'amfoss/static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
