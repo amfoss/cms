69e47f9d39d0e9093328a92ff2b47066d5070574
aswinshenoy <aswinshenoy65@gmail.com>
2019-03-10 18:36:36 +0530

    Update Validator of Image

--- gallery/validators.py
+++ gallery/validators.py
@@ -2,12 +2,13 @@ from django.core.exceptions import ValidationError
 from imagekit.processors import ResizeToFit
 
 def validate_file_size(value):
-    filesize = value.size
+    if value:
+        filesize = value.size
 
-    if filesize > 10485761:
-        raise ValidationError("The maximum file size that can be uploaded is 10MB.")
-    else:
-        return value
+        if filesize > 10485761:
+            raise ValidationError("The maximum file size that can be uploaded is 10MB.")
+        else:
+            return value
 
 processed_image_field_specs = {
     'format': 'JPEG',
