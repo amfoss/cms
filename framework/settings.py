ed21c91ea3e16cd1b59598ea17d979b25de8fcfe
aswinshenoy <aswinshenoy65@gmail.com>
2019-04-17 15:44:41 +0530

    Update Settings

--- framework/settings.py
+++ framework/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from datetime import timedelta
 from framework.local_settings import *
+# from framework.server_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,18 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'framework.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
