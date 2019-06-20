9b591b32128a0cabe9b3010087b5087637882979
ashwin <aswinshenoy65@gmail.com>
2019-06-21 00:30:50 +0530

    Add support for late members, and show past history in telegram status bot

--- status/management/commands/log_status_updates.py
+++ status/management/commands/log_status_updates.py
@@ -41,8 +41,9 @@ class Command(BaseCommand):
                 i += 1
 
         if options['send_telegram_report']:
-            maxt = datetime.now().replace(hour=23, minute=00)
-            mint = datetime.now().replace(hour=18, minute=00)
+            today = datetime.combine(d, datetime.min.time())
+            maxt = today.replace(hour=23, minute=00)
+            mint = today.replace(hour=18, minute=00)
             members_list = Profile.objects.values('user', 'first_name', 'last_name', 'email', 'batch').order_by('batch')
             members_count = Profile.objects.filter(batch__gt=d.year-4).count()
 
@@ -74,13 +75,22 @@ class Command(BaseCommand):
                 message += ln + ' (' + lt.astimezone(timezone('Asia/Kolkata')).strftime('%I:%M %p') + ')\n'
             mf = 0
 
+            #Reports of people who send report lately
+            lateLogs = StatusRegister.objects.filter(timestamp__gt=maxt).order_by('timestamp')
+            if lateLogs.count() > 0:
+                message += '''\n\n<b>Members who were late to sent their status update: </b> \n'''
+            for m in members_list:
+                obj = lateLogs.filter(member=m['user'])
+                if obj:
+                    message += m['first_name'] + ' ' + m['last_name']  + ' [' + str(obj[0].timestamp.astimezone(timezone('Asia/Kolkata')).strftime('%I:%M %p')) + '] \n'
+
             # Reports are generated only for the last 4 batches from current year
             for y in range(d.year, d.year-4, -1):
                 yf = 0
                 for m in members_list:
                     if m['email'] not in log.emails and y == m['batch']:
                         if not mf:
-                            message += '''\n\n<b>Members who didn't sent status updates:</b> \n'''
+                            message += '''\n\n<b>Members who didnt send status updates:</b> \n'''
                             mf = 1
                         if not yf:
                             message += '\n<b>' + str(y) + '</b>\n'
@@ -94,12 +104,26 @@ class Command(BaseCommand):
                         if obj:
                             last = obj[0]
                             diff = d-last.timestamp.date()
-                            message += ' [ ' + str(diff.days) + ' ]'
+                            if diff.days > 7:
+                                message += ' [1W+, '
+                            if diff.days > 14:
+                                message += ' [2W+, '
+                            if diff.days > 21:
+                                message += ' [3W+, '
+                            if diff.days > 28:
+                                message += ' [1M+, '
+                            else:
+                                message += ' [ ' + str(diff.days) + 'D, '
+                            month_ago = d - timedelta(days=31)
+                            count = obj.filter(timestamp__gt=month_ago).count()
+                            message += str(count) + '/31 ]'
+                        else:
+                            message += '[ NSB ]'
                         message += '\n'
             if not mf:
                 message += '\n\n<b>Everyone has send their Status Updates today! &#128079;</b>\n'
 
-            message += '\n<i>This is an automatically generated message.</i>'
+            message += '\n<i>This is an automatically generated message. Please send your status updates daily.</i>'
             print(message)
             bot = telegram.Bot(
                 token=settings.TELEGRAM_BOT_TOKEN)
