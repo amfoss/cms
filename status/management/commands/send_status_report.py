bd439babc0f3c01a9153059185a44239f0d4acae
ashwin <aswinshenoy65@gmail.com>
2019-07-14 06:12:41 +0530

    Fix bug in status report generator

--- status/management/commands/send_status_report.py
+++ status/management/commands/send_status_report.py
@@ -33,35 +33,35 @@ class Command(BaseCommand):
         message = '<b>Status Update Report</b> \n\n'
 
         message += '<b>Most Status Updates: </b>\n\n'
-        i = 1
+        i = 0
         last_val = -1
         for member in desc:
-            profile = Profile.objects.get(id=member)
-            message +=  str(i) + ". " + str(profile.first_name)
-            if(profile.last_name):
-                message +=  ' ' + str(profile.last_name)
-            message += " - " + str(desc[member]) + '\n'
-            if(last_val > desc[member] or last_val<0):
-                i = i+1
-                last_val = desc[member]
+            if last_val > desc[member] or last_val < 0:
+                i =  i+1
                 if i>10:
                     break
+                profile = Profile.objects.get(id=member)
+                message +=  str(i) + ". " + str(profile.first_name)
+                if profile.last_name:
+                    message +=  ' ' + str(profile.last_name)
+                message += " - " + str(desc[member]) + '\n'
+                last_val = desc[member]
 
         message += '\n<b>Least Status Updates: </b>\n\n'
-        i = 1
+        i = 0
         last_val = -1
         for member in asc:
             if asc[member] > 0:
-                profile = Profile.objects.get(id=member)
-                message += str(profile.first_name)
-                if (profile.last_name):
-                    message += ' ' + str(profile.last_name)
-                message += " (" + str(asc[member]) + '), '
-                if (last_val > desc[member] or last_val < 0):
+                if last_val < asc[member]:
                     i = i + 1
-                    last_val = desc[member]
-                    if i > 15:
+                    if i > 10:
                         break
+                    profile = Profile.objects.get(id=member)
+                    message += str(i) + ". " + str(profile.first_name)
+                    if profile.last_name:
+                        message += ' ' + str(profile.last_name)
+                    message += " - " + str(asc[member]) + '\n'
+                    last_val = asc[member]
 
         message += '\n<i>This is an automatically generated message. Please send your status updates daily.</i>'
 
