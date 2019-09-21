577a8c3194035c8be420c8fd5957e5d7a0e909b8
harshithpabbati <pabbatiharshith@gmail.com>
2019-09-21 19:09:54 +0530

    Add status updates API

--- framework/schema.py
+++ framework/schema.py
@@ -4,6 +4,7 @@ import members.schema
 import attendance.schema
 import activity.schema
 import tasks.schema
+import status.schema
 from django.contrib.auth.models import User
 from graphene_django_extras import DjangoObjectField, DjangoListObjectType, DjangoObjectType
 from graphql_jwt.decorators import permission_required, login_required
@@ -13,9 +14,10 @@ class UserType(DjangoObjectType):
     class Meta:
         description = "Type definition for User Object"
         model = User
-        exclude_fields = ( 'id', 'password')
+        exclude_fields = ('id', 'password')
 
-class Query(members.schema.Query, activity.schema.Query, tasks.schema.Query, graphene.ObjectType):
+
+class Query(members.schema.Query, activity.schema.Query, tasks.schema.Query, status.schema.Query, graphene.ObjectType):
     user = graphene.Field(UserType, username=graphene.String(required=False))
 
     @login_required
@@ -24,6 +26,7 @@ class Query(members.schema.Query, activity.schema.Query, tasks.schema.Query, gra
         if username is None:
             username = info.context.user
         return User.objects.get(username=username)
+
 
 class Mutation(members.schema.Mutation, attendance.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
