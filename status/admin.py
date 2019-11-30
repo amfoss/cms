8356ee61f31fa68a256936625f7384533cbbd4fe
Vibhoothi <vibhoothiiaanand@gmail.com>
2019-11-30 13:50:47 +0530

    Revert "Move Status Update Engine to package"
    This reverts commit 2f16d7f948f4f07a2a47d3220fe7cf9ee0adb638.
    
    This revert is due to the wrong architecture created by this bad commit which
    made spurious results
    
    Co-Authored-By: Vibhoothi <vibhoothiiaanand@gmail.com>
    Co-Authored-By: Aniketh Gireesh <anikethgireesh@gmail.com>
    Co-Authored-By: B Krishnan Iyer <krishnaniyer97@gmail.com>
    Co-Authored-By: Siddarth <siddharth.muralee@gmail.com>
    Co-Authored-By: Venu Vardhan <venuvardhanreddytekula8@gmail.com>
    Signed-off-by: Vibhoothi <vibhoothiiaanand@gmail.com>
    Signed-off-by: Aniketh Gireesh <anikethgireesh@gmail.com>
    Signed-off-by: B Krishnan Iyer <krishnaniyer97@gmail.com>
    Signed-off-by: Siddarth <siddharth.muralee@gmail.com>
    Signed-off-by: Venu Vardhan <venuvardhanreddytekula8@gmail.com>

---
+++ status/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from .models import *
+from easy_select2 import select2_modelform
+from import_export.admin import ImportExportModelAdmin, ExportActionMixin
+from import_export import resources
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'isActive', 'enableGroupNotification')
+        }),
+        ('Timings', {
+            'fields': (('days', 'generationTime', 'dueTime', 'logTime'),)
+        }),
+        ('Mailing', {
+            'fields': (('email',), 'threadMessage',)
+        }),
+        ('Reporting', {
+            'fields': ('footerMessage',)
+        }),
+    )
+    list_display = ('name', 'isActive', 'enableGroupNotification', 'generationTime', 'dueTime', 'logTime')
+    search_fields = ['name']
+    select2 = select2_modelform(Thread, attrs={'width': '250px'})
+    form = select2
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    fields = [
+        ('member', 'thread'),
+        ('date', 'timestamp'),
+        'message'
+    ]
+    list_display = ('member', 'date', 'timestamp', 'thread')
+    search_fields = ('member__username', 'thread__name')
+    filter = ('thread',)
+
+
+class DailyLogResource(resources.ModelResource):
+    class Meta:
+        model = DailyLog
+
+
+@admin.register(DailyLog)
+class DailyLogAdmin(ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin):
+    fields = [
+        ('date', 'thread'),
+        'members',
+        'late',
+        'didNotSend'
+    ]
+    list_display = ('date', 'thread', 'totalCount', 'lateCount', 'didNotSendCount')
+    select2 = select2_modelform(DailyLog, attrs={'width': '300px'})
+    form = select2
+    resource_class = DailyLogResource
+
+    @staticmethod
+    def totalCount(obj):
+        return obj.members.count()
+
+    @staticmethod
+    def lateCount(obj):
+        return obj.late.count()
+
+    @staticmethod
+    def didNotSendCount(obj):
+        return obj.didNotSend.count()
---
+++ status/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StatusConfig(AppConfig):
+    name = 'status'
+    verbose_name = "Status Update Module"
