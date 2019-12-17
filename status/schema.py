amfoss/cms

commit bcaa37673f9a07fccf47d06a94a18d5e93d86281

status: Add dailyStatusUpdate API

status/schema.py
@@ -1,7 +1,10 @@
 import graphene
+from graphql_jwt.decorators import login_required
+
 from .models import *
 from django.contrib.auth.models import User
 from framework.api.user import UserBasicObj
+from members.models import Group
 
 
 class MessageObj(graphene.ObjectType):
@@ -20,14 +23,63 @@ class MessageObj(graphene.ObjectType):
         return self['timestamp']
 
 
+class memberSentObj(graphene.ObjectType):
+    member = graphene.Field(UserBasicObj)
+
+    def resolve_member(self, info):
+        return User.objects.values().get(id=self['member_id'])
+
+
+class memberDidNotSendObj(graphene.ObjectType):
+    member = graphene.Field(UserBasicObj)
+
+    def resolve_member(self, info):
+        return User.objects.values().get(id=self['member_id'])
+
+
+class dailyStatusObj(graphene.ObjectType):
+    date = graphene.types.datetime.Date()
+    membersSent = graphene.List(memberSentObj)
+    memberDidNotSend = graphene.List(memberDidNotSendObj)
+
+    def resolve_date(self, info):
+        return self
+
+    def resolve_membersSent(self, info):
+        return Message.objects.values().filter(date=self)
+
+    def resolve_memberDidNotSend(self, info):
+        groups = Group.objects.filter(statusUpdateEnabled=True).values('members__username')
+        messages = Message.objects.values('member__username').filter(date=self)
+        LogUsernames = []
+        for i in messages:
+            LogUsernames.append(i['member__username'])
+        usernames = []
+        for member in groups:
+            username = member['members__username']
+            if username not in LogUsernames:
+                usernames.append(username)
+        return usernames
+
+
 class Query(graphene.ObjectType):
     getStatusUpdates = graphene.List(MessageObj, date=graphene.types.datetime.Date(required=True))
     getMemberStatusUpdates = graphene.List(MessageObj, username=graphene.String(required=True))
+    dailyStatusUpdates = graphene.Field(
+        dailyStatusObj,
+        date=graphene.types.datetime.Date(required=True)
+    )
 
+    @login_required
     def resolve_getStatusUpdates(self, info, **kwargs):
         date = kwargs.get('date')
         return Message.objects.values().filter(date=date)
 
+    @login_required
     def resolve_getMemberStatusUpdates(self, info, **kwargs):
         username = kwargs.get('username')
         return reversed(Message.objects.values().filter(member__username=username))
+
+    @login_required
+    def resolve_dailyStatusUpdates(self, info, **kwargs):
+        return kwargs.get('date')

framework/api/user.py
@@ -1,6 +1,7 @@
 import graphene
 from members.api.profile import AvatarObj
 from members.models import Profile
+from status.models import Message
 
 
 class UserBasicObj(graphene.ObjectType):
@@ -13,6 +14,7 @@ class UserBasicObj(graphene.ObjectType):
     isMembershipActive = graphene.Boolean()
     isAdmin = graphene.Boolean()
     joinDateTime = graphene.types.datetime.DateTime()
+    statusUpdateCount = graphene.Int()
 
     def resolve_firstName(self, info):
         return self['first_name']
@@ -34,3 +36,6 @@ class UserBasicObj(graphene.ObjectType):
 
     def resolve_avatar(self, info):
         return Profile.objects.values().get(user__username=self['username'])
+
+    def resolve_statusUpdateCount(self, info):
+        return Message.objects.values().filter(member__username=self['username']).count()

commit 2c747fa22f94e41c3dedba4d6922c448e46cc6ba

status: Fix the issue in dailyStatusUpdate API
This commit is to fix the regression brought by bcaa37673f9a07fccf47d06a94a18d5e93d86281

status/schema.py
@@ -34,7 +34,7 @@ class memberDidNotSendObj(graphene.ObjectType):
     member = graphene.Field(UserBasicObj)
 
     def resolve_member(self, info):
-        return User.objects.values().get(id=self['member_id'])
+        return User.objects.values().get(username=self)
 
 
 class dailyStatusObj(graphene.ObjectType):