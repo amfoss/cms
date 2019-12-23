5f1e3e8c4d0a0a86d32edf19998da7f584c46367
harshithpabbati <pabbatiharshith@gmail.com>
2019-12-23 14:03:21 +0530

    password: Add viewAccounts API

--- password/admin.py
+++ password/admin.py
@@ -5,8 +5,8 @@ from easy_select2 import select2_modelform
 
 @admin.register(Password)
 class PasswordAdmin(admin.ModelAdmin):
-    fields = [('name', 'login_name', 'password'), 'admins', 'details']
+    fields = [('name', 'login_name', 'password'), ('admins', 'url'), ('key', 'details')]
 
     select2 = select2_modelform(Password, attrs={'width': '500px'})
     form = select2
-    list_display = ['name', 'login_name']
+    list_display = ['name', 'login_name', 'url']
