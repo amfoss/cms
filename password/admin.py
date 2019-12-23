46e360340783c4231885c4f7221ccb06831b98f2
harshithpabbati <pabbatiharshith@gmail.com>
2019-12-23 14:20:58 +0530

    password: Remove key in admin
    This commit is to fix the regression brought by 74d297df3cf02baeaea33e38afc78263858dfd73

--- password/admin.py
+++ password/admin.py
@@ -5,7 +5,7 @@ from easy_select2 import select2_modelform
 
 @admin.register(Password)
 class PasswordAdmin(admin.ModelAdmin):
-    fields = [('name', 'login_name', 'password'), ('admins', 'url'), ('key', 'details')]
+    fields = [('name', 'login_name', 'password'), ('admins', 'url'), 'details']
 
     select2 = select2_modelform(Password, attrs={'width': '500px'})
     form = select2
