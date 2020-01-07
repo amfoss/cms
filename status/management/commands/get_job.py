amfoss/cms

commit bd462ceb7703b1e4619d3f3fa7329e36df1fb9a6

status: Kick Members from the group
To kick members from the telegram group if they don't send status updates for more than 3 days

status/management/commands/get_job.py
@@ -15,6 +15,7 @@ from status.StatusUpdateReporter import ReportMaker
 from status.models import Thread
 from utilities.models import Mailer
 from registration.models import Application
+from members.models import Profile
 
 to_tz = timezone.get_default_timezone()
 
@@ -72,7 +73,8 @@ def sendTelegramReport(thread):
         d = d - timedelta(days=1)
 
     logs = ReportMaker(d, thread.id).message
-
+    shouldKick = ReportMaker(d, thread.id).membersToBeKicked
+    print(shouldKick)
     telegramAgents = []
     groups = Group.objects.filter(thread_id=thread.id, statusUpdateEnabled=True)
     for group in groups:
@@ -87,6 +89,31 @@ def sendTelegramReport(thread):
             text=logs,
             parse_mode=telegram.ParseMode.HTML
         )
+
+        def sendTelegramReport(thread):
+            d = date.today()
+            if thread.generationTime > thread.logTime:
+                d = d - timedelta(days=1)
+
+            logs = ReportMaker(d, thread.id).message
+            shouldKick = ReportMaker(d, thread.id).membersToBeKicked
+            telegramAgents = []
+            groups = Group.objects.filter(thread_id=thread.id, statusUpdateEnabled=True)
+            for group in groups:
+                obj = [group.telegramBot, group.telegramGroup]
+                if obj not in telegramAgents:
+                    telegramAgents.append(obj)
+
+            for agent in telegramAgents:
+                bot = telegram.Bot(token=agent[0])
+                bot.send_message(
+                    chat_id=agent[1],
+                    text=logs,
+                    parse_mode=telegram.ParseMode.HTML
+                )
+                for user in shouldKick:
+                    profile = Profile.objects.get(user=user)
+                    bot.kick_chat_member(chat_id=agent[1], user_id=profile.telegram_id)
 
 
 class Command(BaseCommand):

status/StatusUpdateReporter.py
@@ -12,6 +12,7 @@ class ReportMaker(object):
         self.date = d
         self.thread = Thread.objects.get(id=thread_id)
         self.message = self.generateDailyReport()
+        self.membersToBeKicked = self.kickMembers()
 
     @staticmethod
     def getPercentageSummary(send, total):
@@ -74,11 +75,19 @@ class ReportMaker(object):
             message += str(diff) + 'D'
         return message
 
+    @staticmethod
+    def getLastSend(last_send, expected_date):
+        diff = expected_date - last_send.date()
+        diff = diff.days + 1
+        return diff
+
     def getMemberLastRequiredDate(self, member):
-        return DailyLog.objects.filter(thread=self.thread, members=member, date__lt=self.date).order_by('-date').first().date
+        return DailyLog.objects.filter(thread=self.thread, members=member, date__lt=self.date).order_by(
+            '-date').first().date
 
     def getMemberLastSend(self, member):
-        obj = DailyLog.objects.filter(thread=self.thread, members=member, date__lt=self.date).exclude(didNotSend=member).order_by('-date').first()
+        obj = DailyLog.objects.filter(thread=self.thread, members=member, date__lt=self.date).exclude(
+            didNotSend=member).order_by('-date').first()
         if obj:
             return Message.objects.filter(thread=self.thread, member=member, date=obj.date)[0].timestamp
         else:
@@ -99,7 +108,7 @@ class ReportMaker(object):
             message = '\n<b>' + self.getBatchName(year) + ' (' + str(m.count()) + ')' + '</b>\n\n'
             i = 0
             for member in m:
-                i = i+1
+                i = i + 1
                 lastSend = self.getMemberLastSend(member.user)
                 message += str(i) + '. ' + self.getName(member.user)
                 if lastSend:
@@ -159,6 +168,26 @@ class ReportMaker(object):
                 message += '\n<i>' + thread.footerMessage + '</i>'
 
             return message
+
+        except ObjectDoesNotExist:
+            raise
+
+    def kickMembers(self):
+        shouldKick = []
+        date = self.date
+        thread = self.thread
+        try:
+            log = DailyLog.objects.get(date=date, thread=thread)
+            members = log.didNotSend.all()
+            for member in members:
+                lastSend = self.getMemberLastSend(member)
+                if lastSend:
+                    lastSend = self.getLastSend(lastSend,
+                                                self.getMemberLastRequiredDate(member))
+                    if lastSend > 3:
+                        shouldKick.append(member)
+
+            return shouldKick
 
         except ObjectDoesNotExist:
             raise

commit bb6cea6486cc56def510658da2b8deab6c25e098

status: Fix issue in sendTelegramReport
This commit is to fix the regression caused by bd462ceb7703b1e4619d3f3fa7329e36df1fb9a6

status/management/commands/get_job.py
@@ -74,7 +74,6 @@ def sendTelegramReport(thread):
 
     logs = ReportMaker(d, thread.id).message
     shouldKick = ReportMaker(d, thread.id).membersToBeKicked
-    print(shouldKick)
     telegramAgents = []
     groups = Group.objects.filter(thread_id=thread.id, statusUpdateEnabled=True)
     for group in groups:
@@ -89,31 +88,9 @@ def sendTelegramReport(thread):
             text=logs,
             parse_mode=telegram.ParseMode.HTML
         )
-
-        def sendTelegramReport(thread):
-            d = date.today()
-            if thread.generationTime > thread.logTime:
-                d = d - timedelta(days=1)
-
-            logs = ReportMaker(d, thread.id).message
-            shouldKick = ReportMaker(d, thread.id).membersToBeKicked
-            telegramAgents = []
-            groups = Group.objects.filter(thread_id=thread.id, statusUpdateEnabled=True)
-            for group in groups:
-                obj = [group.telegramBot, group.telegramGroup]
-                if obj not in telegramAgents:
-                    telegramAgents.append(obj)
-
-            for agent in telegramAgents:
-                bot = telegram.Bot(token=agent[0])
-                bot.send_message(
-                    chat_id=agent[1],
-                    text=logs,
-                    parse_mode=telegram.ParseMode.HTML
-                )
-                for user in shouldKick:
-                    profile = Profile.objects.get(user=user)
-                    bot.kick_chat_member(chat_id=agent[1], user_id=profile.telegram_id)
+        for user in shouldKick:
+            profile = Profile.objects.get(user=user)
+            bot.kick_chat_member(chat_id=agent[1], user_id=profile.telegram_id)
 
 
 class Command(BaseCommand):