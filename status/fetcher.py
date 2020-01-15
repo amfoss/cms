amfoss/cms

commit e5460292a03d3577234788957f6e6938b46187c6

status: Fetch only valid status updates
- Fetches mail only if it's sent to ML
- Update Telegram Report

status/fetcher.py
@@ -56,9 +56,16 @@ class GMailFetcher(object):
 
     def processMessages(self, msgs):
         log = []
+        CCemail = None
         for msg in msgs:
             header_data = msg["payload"]["headers"]
             for data in header_data:
+                if "To" == data["name"]:
+                    to_email = data["value"]
+                    if '<' in to_email:
+                        start = to_email.find('<')
+                        end = to_email.find('>')
+                        to_email = to_email[start + 1: end]
                 if "From" == data["name"]:
                     email_id = data["value"]
                     if '<' in email_id:
@@ -68,6 +75,12 @@ class GMailFetcher(object):
                 if "Received" == data["name"]:
                     timestamp = parsedate_to_datetime(data["value"].split(';', 1)[-1]).astimezone(
                         pytz.timezone("Asia/Calcutta"))
+                if "Cc" == data["name"]:
+                    CCemail = data["value"]
+                    if '<' in CCemail:
+                        start = CCemail.find('<')
+                        end = CCemail.find('>')
+                        CCemail = CCemail[start + 1: end]
             MsgB64 = ""
             try:
                 if "parts" in msg["payload"]:
@@ -85,6 +98,8 @@ class GMailFetcher(object):
             Msg = Msg.split("-- <br />You received this message")[0]
 
             log.append({
+                "CCemail": CCemail,
+                "to": to_email,
                 'email': email_id,
                 'date': self.date,
                 'timestamp': timestamp.isoformat(),

status/logger.py
@@ -14,32 +14,46 @@ def log(data, members, thread_id):
             timestamp = dateutil.parser.parse(entry['timestamp'])
             logDate = dateutil.parser.parse(entry['date']).date()
 
-            msgObj, msgCreated = Message.objects.get_or_create(
-                member=user,
-                date=logDate,
-                timestamp=timestamp,
-                thread=thread,
-                message=entry['message']
-            )
-            msgObj.save()
-            if msgCreated:
+            if entry['to'] == thread.email and entry['CCemail'] == thread.email:
+                msgObj, msgCreated = Message.objects.get_or_create(
+                    member=user,
+                    date=logDate,
+                    timestamp=timestamp,
+                    thread=thread,
+                    message=entry['message']
+                )
+                msgObj.save()
+                if msgCreated:
+                    obj, created = DailyLog.objects.get_or_create(
+                        date=logDate,
+                        thread=thread,
+                    )
+                    if created:
+                        obj.members.add(*members)
+                        obj.didNotSend.add(*members)
+
+                    if user in obj.members.all():
+                        dueTime = timezone("Asia/Calcutta").localize(
+                            dateutil.parser.parse(entry['date']).replace(hour=int(thread.dueTime[:2]),
+                                                                         minute=int(thread.dueTime[2:])))
+
+                        if user in obj.didNotSend.all():
+                            if timestamp > dueTime:
+                                obj.late.add(user)
+                            obj.didNotSend.remove(user)
+                    obj.save()
+            else:
                 obj, created = DailyLog.objects.get_or_create(
                     date=logDate,
                     thread=thread,
                 )
+
                 if created:
                     obj.members.add(*members)
                     obj.didNotSend.add(*members)
 
-                if user in obj.members.all():
-                    dueTime = timezone("Asia/Calcutta").localize(
-                        dateutil.parser.parse(entry['date']).replace(hour=int(thread.dueTime[:2]),
-                                                                     minute=int(thread.dueTime[2:])))
-
-                    if user in obj.didNotSend.all():
-                        if timestamp > dueTime:
-                            obj.late.add(user)
-                        obj.didNotSend.remove(user)
+                obj.invalidUpdates.add(user)
+                obj.didNotSend.remove(user)
                 obj.save()
 
         except ObjectDoesNotExist:

status/StatusUpdateReporter.py
@@ -138,7 +138,7 @@ class ReportMaker(object):
         lateCount = late_members.count()
         message = ''
         if lateCount > 0:
-            message += '\n\n<b>&#8987; LATE (' + str(lateCount) + ') : </b> \n'
+            message += '\n\n<b>&#8987; LATE (' + str(lateCount) + ') : </b> \n\n'
             i = 0
             for member in late_members.all():
                 i = i + 1
@@ -146,6 +146,17 @@ class ReportMaker(object):
                     '-timestamp').first().timestamp
                 message += str(i) + '. ' + self.getName(member) + ' [' + timestamp.astimezone(
                     timezone('Asia/Kolkata')).strftime('%I:%M %p') + '] \n'
+        return message
+
+    def getInvalidUpdatesReport(self, invalidUpdates):
+        invalidUpdatesCount = invalidUpdates.count()
+        message = ''
+        if invalidUpdatesCount > 0:
+            message += '\n\n<b>⚠️ INVALID STATUS UPDATES (' + str(invalidUpdatesCount) + ') : </b> \n\n'
+            i = 0
+            for member in invalidUpdates.all():
+                i = i + 1
+                message += str(i) + '. ' + self.getName(member) + '\n'
         return message
 
     def generateDailyReport(self):
@@ -156,12 +167,14 @@ class ReportMaker(object):
 
             totalMembers = log.members.count()
             didNotSendCount = log.didNotSend.count()
-            sendCount = totalMembers - didNotSendCount
+            invalidUpdatesCount = log.invalidUpdates.count()
+            sendCount = totalMembers - (didNotSendCount + invalidUpdatesCount)
 
             message = '<b>Daily Status Update Report</b> \n\n &#128197; ' + date.strftime(
                 '%d %B %Y') + ' | &#128228; ' + str(sendCount) + '/' + str(totalMembers) + ' Members'
 
             message += '\n\n<b>' + self.getPercentageSummary(sendCount, totalMembers) + '</b>'
+            message += self.getInvalidUpdatesReport(log.invalidUpdates)
             message += self.getLateReport(log.late)
             message += self.generateDidNotSendReport(log.didNotSend)
             if thread.footerMessage:

status/admin.py
@@ -57,9 +57,10 @@ class DailyLogAdmin(ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin)
         ('date', 'thread'),
         'members',
         'late',
-        'didNotSend'
+        'didNotSend',
+        'invalidUpdates'
     ]
-    list_display = ('date', 'thread', 'totalCount', 'lateCount', 'didNotSendCount')
+    list_display = ('date', 'thread', 'totalCount', 'lateCount', 'didNotSendCount', 'invalidUpdatesCount')
     select2 = select2_modelform(DailyLog, attrs={'width': '300px'})
     form = select2
     resource_class = DailyLogResource
@@ -75,3 +76,7 @@ class DailyLogAdmin(ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin)
     @staticmethod
     def didNotSendCount(obj):
         return obj.didNotSend.count()
+
+    @staticmethod
+    def invalidUpdatesCount(obj):
+        return obj.invalidUpdates.count()

status/models.py
@@ -59,6 +59,7 @@ class DailyLog(models.Model):
     members = models.ManyToManyField(User, related_name='threadMembers', blank=True)
     late = models.ManyToManyField(User, related_name='lateStatusLog', blank=True)
     didNotSend = models.ManyToManyField(User, related_name='didNotSendStatusLog', blank=True)
+    invalidUpdates = models.ManyToManyField(User, related_name='invalidStatusLog', blank=True)
 
     class Meta:
         verbose_name_plural = "Daily Logs"

commit 4aa8b9f5f6cdbd53912020cb2527f161bc218d2d

status: Add CC Checker in status updates
Fixes the regression caused by #110, #111

status/fetcher.py
@@ -56,8 +56,9 @@ class GMailFetcher(object):
 
     def processMessages(self, msgs):
         log = []
-        CCemail = None
         for msg in msgs:
+            CCemail = None
+            to_email = None
             header_data = msg["payload"]["headers"]
             for data in header_data:
                 if "To" == data["name"]:

status/logger.py
@@ -14,7 +14,7 @@ def log(data, members, thread_id):
             timestamp = dateutil.parser.parse(entry['timestamp'])
             logDate = dateutil.parser.parse(entry['date']).date()
 
-            if entry['to'] == thread.email:
+            if entry['to'] == thread.email and entry['CCemail'] is None:
                 msgObj, msgCreated = Message.objects.get_or_create(
                     member=user,
                     date=logDate,