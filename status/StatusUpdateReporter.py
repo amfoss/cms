cdd1ff0bfc2fdbe0e25a5b7a744bed1412aae0fa
harshithpabbati <pabbatiharshith@gmail.com>
2020-01-22 14:49:05 +0530

    status: Exclude invalid Status Updates
    - Excludes invalid status updates in getMemberLastSend function

--- status/StatusUpdateReporter.py
+++ status/StatusUpdateReporter.py
@@ -87,7 +87,7 @@ class ReportMaker(object):
 
     def getMemberLastSend(self, member):
         obj = DailyLog.objects.filter(thread=self.thread, members=member, date__lt=self.date).exclude(
-            didNotSend=member).order_by('-date').first()
+            didNotSend=member).exclude(invalidUpdates=member).order_by('-date').first()
         if obj:
             return Message.objects.filter(thread=self.thread, member=member, date=obj.date)[0].timestamp
         else:
