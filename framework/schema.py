amfoss/cms

commit a7050aa2d1f6a78b2cbc7ff204641505e744a1f1

framework: Update users API
- Fetches only active users

framework/schema.py
@@ -218,6 +218,7 @@ class Query(
 ):
     user = graphene.Field(UserObj, username=graphene.String(required=True))
     users = graphene.List(UserObj, sort=graphene.String())
+    activeUsers = graphene.List(UserObj, sort=graphene.String())
     isClubMember = graphene.Boolean()
     getInActiveUsers = graphene.List(UserType)
 
@@ -233,6 +234,12 @@ class Query(
         if sort is None:
             sort = 'username'
         return User.objects.values().all().order_by(sort)
+
+    def resolve_activeUsers(self, info, **kwargs):
+        sort = kwargs.get('sort')
+        if sort is None:
+            sort = 'username'
+        return User.objects.filter(is_active=True).order_by(sort)
 
     def resolve_isClubMember(self, info, **kwargs):
         user = info.context.user

commit ed1ad2795dd25e974859f19b4ccb3aef26c64ff6

framework: Fix activeUsers API
- Fix `User object is not subscriptable` error

This commit is to fix the regression caused by a7050aa2d1f6a78b2cbc7ff204641505e744a1f1

framework/schema.py
@@ -239,7 +239,7 @@ class Query(
         sort = kwargs.get('sort')
         if sort is None:
             sort = 'username'
-        return User.objects.filter(is_active=True).order_by(sort)
+        return User.objects.values().filter(is_active=True).order_by(sort)
 
     def resolve_isClubMember(self, info, **kwargs):
         user = info.context.user