amfoss/cms

commit 459a41ead34687415ca8a3bfa0b561e7cef498f3

Integrate Cloudflare to add/remove user for forwarding email

framework/platforms/cloudflare.py
@@ -1,0 +1,65 @@
+import CloudFlare
+from framework import settings
+from framework.platforms.userPlatform import UserPlatform
+
+EMAIL_USER = settings.EMAIL_HOST_USER
+CLOUDFLARE_TOKEN = settings.CLOUDFLARE_TOKEN
+CLOUDFLARE_ZONE_ID = settings.CLOUDFLARE_ZONE_ID
+
+
+class Cloudflare(UserPlatform):
+
+    def __init__(self, email, customEmail=None):
+        self.email = email
+        if customEmail is None:
+            self.customEmail = ""
+        else:
+            self.customEmail = customEmail
+
+    def removeUser(self):
+        cf = CloudFlare.CloudFlare(email=EMAIL_USER, token=CLOUDFLARE_TOKEN)
+        records = cf.zones.dns_records.get(CLOUDFLARE_ZONE_ID, params={'per_page': 50})
+        for record in records:
+            if record['type'] == 'TXT' and record['name'] == 'amfoss.in' and record['content'].startswith(
+                    'forward-email'):
+                content = record['content'].split(",")
+                for data in content:
+                    if data.find(self.email) != -1:
+                        index = content.index(data)
+                        if index != 0:
+                            content.pop(index)
+                            record['content'] = ", ".join(content)
+                        else:
+                            content[1] = "forward-email=" + content[1]
+                            content = content[1:]
+                            record['content'] = ", ".join(content)
+
+                        dns_record = {
+                            'name': record['name'],
+                            'type': record['type'],
+                            'content': record['content'],
+                            'proxied': record['proxied']
+                        }
+                        try:
+                            dns_record = cf.zones.dns_records.put(CLOUDFLARE_ZONE_ID, record['id'], data=dns_record)
+                        except CloudFlare.exceptions.CloudFlareAPIError as e:
+                            pass
+
+    def addUser(self):
+        cf = CloudFlare.CloudFlare(email=EMAIL_USER, token=CLOUDFLARE_TOKEN)
+        records = cf.zones.dns_records.get(CLOUDFLARE_ZONE_ID, params={'per_page': 50})
+        for record in records:
+            if record['type'] == 'TXT' and record['name'] == 'amfoss.in' and record['content'].startswith(
+                    'forward-email'):
+                newContent = record['content'] + ", " + self.customEmail + ":" + self.email
+                if len(newContent) < 255:
+                    dns_record = {
+                        'name': record['name'],
+                        'type': record['type'],
+                        'content': newContent,
+                        'proxied': record['proxied']
+                    }
+                    try:
+                        dns_record = cf.zones.dns_records.put(CLOUDFLARE_ZONE_ID, record['id'], data=dns_record)
+                    except CloudFlare.exceptions.CloudFlareAPIError as e:
+                        pass

framework/api/mutation.py
@@ -5,6 +5,7 @@ from .APIException import APIException
 
 from framework.platforms.gitlab import GitLab
 from framework.platforms.github import GitHub
+from framework.platforms.cloudflare import Cloudflare
 
 
 class statusObj(graphene.ObjectType):
@@ -83,6 +84,42 @@ class RemoveUserGitHub(graphene.Mutation):
                                code='ONLY_SUPERUSER_HAS_ACCESS')
 
 
+class InviteUserCloudflare(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+
+    Output = statusObj
+
+    def mutate(self, info, username):
+        if info.context.user.is_superuser:
+            user = User.objects.get(username=username)
+            profile = Profile.objects.get(user=user)
+            cloudflare = Cloudflare(profile.email, profile.customEmail)
+            cloudflare.addUser()
+            return statusObj(status='Done')
+        else:
+            raise APIException('Only Superusers have access',
+                               code='ONLY_SUPERUSER_HAS_ACCESS')
+
+
+class RemoveUserCloudflare(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+
+    Output = statusObj
+
+    def mutate(self, info, username):
+        if info.context.user.is_superuser:
+            user = User.objects.get(username=username)
+            profile = Profile.objects.get(user=user)
+            cloudflare = Cloudflare(profile.email)
+            cloudflare.removeUser()
+            return statusObj(status='Done')
+        else:
+            raise APIException('Only Superusers have access',
+                               code='ONLY_SUPERUSER_HAS_ACCESS')
+
+
 class MakeUserInActive(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
@@ -97,6 +134,8 @@ class MakeUserInActive(graphene.Mutation):
             gitlab.removeUser()
             github = GitHub(profile.githubUsername)
             github.removeUser()
+            cloudflare = Cloudflare(profile.email)
+            cloudflare.removeUser()
             user.is_active = False
             user.save()
             return statusObj(status='Done')
@@ -119,6 +158,8 @@ class MakeUserActive(graphene.Mutation):
             gitlab.addUser()
             github = GitHub(profile.githubUsername)
             github.addUser()
+            cloudflare = Cloudflare(profile.email, profile.customEmail)
+            cloudflare.addUser()
             user.is_active = True
             user.save()
             return statusObj(status='Done')
@@ -132,5 +173,7 @@ class Mutation(object):
     invite_user_gitlab = InviteUserGitLab.Field()
     remove_user_github = RemoveUserGitHub.Field()
     invite_user_github = InviteUserGitHub.Field()
+    remove_user_cloudflare = RemoveUserCloudflare.Field()
+    invite_user_cloudflare = InviteUserCloudflare.Field()
     make_user_in_active = MakeUserInActive.Field()
     make_user_active = MakeUserActive.Field()

framework/local_settings.py
@@ -26,3 +26,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 GITLAB_TOKEN = 'GITLAB_TOKEN'
 GITHUB_TOKEN = 'GITHUB_TOKEN'
+CLOUDFLARE_TOKEN = 'CLOUDFLARE_TOKEN'
+CLOUDFLARE_ZONE_ID = 'CLOUDFLARE_ZONE_ID'

commit cbf98246717fc76e1e17a3bb48457913db57dc46

platforms: cloudflare: Add break condition
- Breaks when it is added for once

This commit to fix the regression caused by 459a41ead34687415ca8a3bfa0b561e7cef498f3

framework/platforms/cloudflare.py
@@ -63,3 +63,4 @@ class Cloudflare(UserPlatform):
                         dns_record = cf.zones.dns_records.put(CLOUDFLARE_ZONE_ID, record['id'], data=dns_record)
                     except CloudFlare.exceptions.CloudFlareAPIError as e:
                         pass
+                    break