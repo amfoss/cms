a2506dbeba4a97e9394f7c4d8687010cbf8b58d0
harshithpabbati <pabbatiharshith@gmail.com>
2020-04-02 21:16:09 +0530

    Integrate GitHub to add/remove user from amFOSS Org

--- framework/api/mutation.py
+++ framework/api/mutation.py
@@ -4,6 +4,7 @@ from members.models import Profile
 from .APIException import APIException
 
 from framework.platforms.gitlab import GitLab
+from framework.platforms.github import GitHub
 
 
 class statusObj(graphene.ObjectType):
@@ -46,6 +47,42 @@ class RemoveUserGitLab(graphene.Mutation):
                                code='ONLY_SUPERUSER_HAS_ACCESS')
 
 
+class InviteUserGitHub(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+
+    Output = statusObj
+
+    def mutate(self, info, username):
+        if info.context.user.is_superuser:
+            user = User.objects.get(username=username)
+            profile = Profile.objects.get(user=user)
+            github = GitHub(profile.githubUsername)
+            github.addUser()
+            return statusObj(status='Done')
+        else:
+            raise APIException('Only Superusers have access',
+                               code='ONLY_SUPERUSER_HAS_ACCESS')
+
+
+class RemoveUserGitHub(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+
+    Output = statusObj
+
+    def mutate(self, info, username):
+        if info.context.user.is_superuser:
+            user = User.objects.get(username=username)
+            profile = Profile.objects.get(user=user)
+            github = GitHub(profile.githubUsername)
+            github.removeUser()
+            return statusObj(status='Done')
+        else:
+            raise APIException('Only Superusers have access',
+                               code='ONLY_SUPERUSER_HAS_ACCESS')
+
+
 class MakeUserInActive(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
@@ -58,6 +95,8 @@ class MakeUserInActive(graphene.Mutation):
             profile = Profile.objects.get(user=user)
             gitlab = GitLab(profile.gitlabUsername)
             gitlab.removeUser()
+            github = GitHub(profile.githubUsername)
+            github.removeUser()
             user.is_active = False
             user.save()
             return statusObj(status='Done')
@@ -78,6 +117,8 @@ class MakeUserActive(graphene.Mutation):
             profile = Profile.objects.get(user=user)
             gitlab = GitLab(profile.gitlabUsername)
             gitlab.addUser()
+            github = GitHub(profile.githubUsername)
+            github.addUser()
             user.is_active = True
             user.save()
             return statusObj(status='Done')
@@ -89,5 +130,7 @@ class MakeUserActive(graphene.Mutation):
 class Mutation(object):
     remove_user_gitlab = RemoveUserGitLab.Field()
     invite_user_gitlab = InviteUserGitLab.Field()
+    remove_user_github = RemoveUserGitHub.Field()
+    invite_user_github = InviteUserGitHub.Field()
     make_user_in_active = MakeUserInActive.Field()
     make_user_active = MakeUserActive.Field()
---
+++ framework/platforms/github.py
@@ -0,0 +1,25 @@
+from github import Github
+from framework import settings
+from framework.platforms.userPlatform import UserPlatform
+
+GITHUB_TOKEN = settings.GITHUB_TOKEN
+
+
+class GitHub(UserPlatform):
+
+    def __init__(self, username):
+        self.username = username
+
+    def removeUser(self):
+        g = Github(GITHUB_TOKEN)
+        ghuser = g.get_user(self.username)
+        org = g.get_organization('amfoss')
+        if org.has_in_members(ghuser):
+            org.remove_from_members(ghuser)
+
+    def addUser(self):
+        g = Github(GITHUB_TOKEN)
+        ghuser = g.get_user(self.username)
+        org = g.get_organization('amfoss')
+        if not org.has_in_members(ghuser):
+            org.add_to_public_members(ghuser)
