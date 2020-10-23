fab4f36c5518499409778dd7e337f6a072ea56b4
harshithpabbati <pabbatiharshith@gmail.com>
2020-10-24 00:43:23 +0530

    settings: change expiry time for JWT Token

--- framework/settings.py
+++ framework/settings.py
@@ -132,6 +132,6 @@ GRAPHQL_JWT = {
     'JWT_ALLOW_ARGUMENT': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
-    'JWT_EXPIRATION_DELTA': timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': timedelta(minutes=5),
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
 }
